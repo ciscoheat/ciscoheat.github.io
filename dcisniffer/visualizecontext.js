23b4d02d49260d4753ec5f210e89c2fe3b6064b8
Andreas <ciscoheat@gmail.com>
2021-10-05 10:54:34 +0200

    Converted to typescript.

--- dcisniffer/visualizecontext.js
+++ dcisniffer/visualizecontext.js
@@ -1,36 +1,35 @@
+var Clicks;
+(function (Clicks) {
+    Clicks[Clicks["Single"] = 1] = "Single";
+    Clicks[Clicks["Double"] = 2] = "Double";
+    Clicks[Clicks["Triple"] = 3] = "Triple";
+})(Clicks || (Clicks = {}));
 class VisualizeContext {
     constructor(nodes, edges, container) {
         //this.roles = new Set(nodes.map(node => node.group))
-
-        this.nodes = new vis.DataSet(nodes.map((node, index, arr) => {
+        const nodeSet = this.nodes = new vis.DataSet(nodes.map((node, index, arr) => {
             const angle = 2 * Math.PI * (index / arr.length + 0.75);
-            const radius = 225 + arr.length * 10
-
-            const output = Object.assign({}, node)
+            const radius = 225 + arr.length * 10;
+            const output = Object.assign({}, node);
             output.x = radius * Math.cos(angle);
             output.y = radius * Math.sin(angle);
             return output;
-        }))
-
-        this.edges = new vis.DataSet(
-            edges.map(e => Object.assign({}, e))
-        )
-
+        }));
+        const edgeSet = this.edges = new vis.DataSet(edges.map(e => Object.assign({}, e)));
         // Set node border and size based on connected edges        
-        this.nodes.updateOnly(this.nodes.get()
-        .map(node => {
-            const nodeEdgesFrom = this.edges.get({filter: e => e.from == node.id})
-            const nodeEdgesTo = this.edges.get({filter: e => e.to == node.id})
-            const uniqueEdgesTo = (edges) => new Set(edges.map(e => e.from + e.to))
-
+        nodeSet.update(nodeSet.get()
+            .map(node => {
+            const nodeEdgesFrom = edgeSet.get({ filter: e => e.from == node.id });
+            const nodeEdgesTo = edgeSet.get({ filter: e => e.to == node.id });
+            const uniqueEdges = (edges) => new Set(edges.map(e => e.from + e.to));
+            const borderWidth = uniqueEdges(nodeEdgesTo).size * 1.5;
             return {
                 id: node.id,
-                borderWidth: uniqueEdgesTo(nodeEdgesTo).size * 1.5,
-                borderWidthSelected: uniqueEdgesTo(nodeEdgesTo).size * 1.5,
+                borderWidth: borderWidth,
+                borderWidthSelected: borderWidth,
                 size: 20 + nodeEdgesFrom.length * 3
-            }
-        }))
-
+            };
+        }));
         const options = {
             physics: false,
             nodes: {
@@ -40,7 +39,8 @@ class VisualizeContext {
                 }
             },
             edges: {
-                arrows: 'to'
+                arrows: 'to',
+                selectionWidth: width => Math.max(3, width * 1.5)
             },
             groups: {
                 '__CONTEXT': {
@@ -54,106 +54,87 @@ class VisualizeContext {
                     }
                 }
             }
-        }
-
+        };
         this.network = new vis.Network(container, {
-            nodes: this.nodes,
-            edges: this.edges
-        }, options)
-
-        // State
-        this._lastClick = [0, 0]
-        
+            nodes: nodeSet,
+            edges: edgeSet
+        }, options);
+        this.clicks = [0, 0];
     } // end constructor
-
     ///// System operations /////////////////////////////////////////
-
     start() {
-        this.network.on("click", params => this.network_addToSelection(params))
+        const network = this.network;
+        network.on("click", params => this.network_addToSelection(params));
     }
-
-    ///// Roles /////////////////////////////////////////////////////
-
-    ///// edges ///////////////////////////////////////////
-
-    edges_display(edgeIds = null, display = true) {
-        if(edgeIds === null) 
-            edgeIds = this.edges.get().map(e => e.id)
-
-        this.edges.updateOnly(
-            edgeIds.map(id => ({
-                id: id,
-                hidden: !display
-            }))
-        )
+    edges_displayAll() {
+        this.edges_display(null);
     }
-
-    ///// network /////////////////////////////////////////
-
+    edges_hideAll() {
+        this.edges_display(null, false);
+    }
+    edges_display(edgeIds, display = true) {
+        if (edgeIds == null)
+            edgeIds = this.edges.get().map(e => e.id);
+        this.edges.update(edgeIds.map(id => ({
+            id: id,
+            hidden: !display
+        })));
+    }
+    clicks_track() {
+        const now = Date.now();
+        let nrClicks = Clicks.Single;
+        if (now - this.clicks[1] < 600)
+            nrClicks = Clicks.Triple;
+        else if (now - this.clicks[0] < 500)
+            nrClicks = Clicks.Double;
+        this.clicks.unshift(now);
+        this.clicks.pop();
+        return nrClicks;
+    }
     network_addToSelection(params) {
-        // Check clicks
-        const isDoubleClick = Date.now() - this._lastClick[0] < 500
-        const isTripleClick = isDoubleClick && Date.now() - this._lastClick[1] < 600
-        this._lastClick.unshift(Date.now())
-        this._lastClick.pop()
-
-        const nodeId = this.network.getNodeAt(params.pointer.DOM)
-        const edgeId = this.network.getEdgeAt(params.pointer.DOM)
-
-        if(!nodeId && !edgeId) {
-            this.edges_display(null, true)
-            return
+        const nodeId = this.network.getNodeAt(params.pointer.DOM);
+        const edgeId = this.network.getEdgeAt(params.pointer.DOM);
+        if (!nodeId && !edgeId) {
+            this.edges_displayAll();
+            return;
         }
-
         // Hide all edges before displaying the selected ones
-        this.edges_display(null, false)
-
-        if(nodeId && isTripleClick) {
-            this.edges_display(this.nodes_uniPathFrom(nodeId))
-        } else {
-            this.nodes_displayEdgesFor(
-                nodeId ? [nodeId] : this.network.getConnectedNodes(edgeId),
-                isDoubleClick
-            )
-        }        
+        this.edges_hideAll();
+        const clicks = this.clicks_track();
+        if (nodeId && clicks == Clicks.Triple) {
+            this.edges_display(this.nodes_uniPathFrom(nodeId));
+        }
+        else {
+            this.nodes_displayEdgesFor(nodeId
+                ? [nodeId]
+                : this.network.getConnectedNodes(edgeId), clicks == Clicks.Double);
+        }
     }
-
     network_connectedEdges(nodeId) {
-        return this.network.getConnectedEdges(nodeId)
+        return this.network.getConnectedEdges(nodeId);
     }
-
-    ///// nodes ///////////////////////////////////////////
-
     nodes_displayEdgesFor(nodeIdList, onlyExactNodes) {
-        const nodes = this.nodes.get(nodeIdList)
-
+        const nodes = this.nodes.get(nodeIdList);
         const filter = onlyExactNodes
             ? n => nodes.some(n2 => n2.id == n.id)
-            : n => nodes.some(selected => selected.group == n.group)
-
-        this.nodes__filterAndDisplayEdges(filter)
+            : n => nodes.some(selected => selected.group == n.group);
+        this.nodes__filterAndDisplayEdges(filter);
     }
-
     nodes__filterAndDisplayEdges(filter, visible = true) {
         const edges = this.nodes
-        .get({ filter: filter || (n => true) })
-        .map(n => n.id)
-        .flatMap(id => this.network_connectedEdges(id))
-
-        this.edges_display(edges, visible)
+            .get({ filter: filter })
+            .map(n => n.id)
+            .flatMap(id => this.network_connectedEdges(id));
+        this.edges_display(edges, visible);
     }
-
     nodes_uniPathFrom(nodeId, visitedIds = []) {
-        visitedIds.push(nodeId)
-
+        visitedIds.push(nodeId);
         const fromEdges = this.edges
-        .get(this.network_connectedEdges(nodeId))
-        .filter(e => e.from == nodeId)
-
-        return fromEdges.map(e => e.id).concat(
-            fromEdges
+            .get(this.network_connectedEdges(nodeId))
+            .filter(e => e.from == nodeId);
+        return fromEdges.map(e => e.id).concat(fromEdges
             .filter(e => !visitedIds.includes(e.to))
-            .flatMap(e => this.nodes_uniPathFrom(e.to, visitedIds))
-        )
+            .flatMap(e => this.nodes_uniPathFrom(e.to, visitedIds)));
     }
 }
+//# sourceMappingURL=visualizecontext.js.map
